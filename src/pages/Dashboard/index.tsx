1d2394fc066a1c6c80c81405e657a4b8ca800fcc
Wanderson Oliveira <testepedroca@gmail.com>
2020-12-01 18:22:18 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logo from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositorioes no Github</Title>;
+  return (
+    <>
+      <img src={logo} alt="Github logo"/>
+      <Title>Explore repositorioes no Github</Title>
+
+       <Form>
+          <input placeholder="Digite o nome do repositório"/>
+          <button type="submit">Pesquisar</button>
+       </Form>
+
+       <Repositories>
+         <a href="teste">
+            <img src="https://avatars1.githubusercontent.com/u/62625309?s=460&u=ff8bc1eacb90d3135b28a5bf8e72f4a5d2966b51&v=4" alt="Wanderson Oliveira"/>
+            <div>
+              <strong>rocketseat/unform</strong>
+              <p>Easy peasy highly scalable ReactJs & React Native forms</p>
+            </div>
+
+            <FiChevronRight size={20} />
+         </a>
+
+         <a href="teste">
+            <img src="https://avatars1.githubusercontent.com/u/62625309?s=460&u=ff8bc1eacb90d3135b28a5bf8e72f4a5d2966b51&v=4" alt="Wanderson Oliveira"/>
+            <div>
+              <strong>rocketseat/unform</strong>
+              <p>Easy peasy highly scalable ReactJs & React Native forms</p>
+            </div>
+
+            <FiChevronRight size={20} />
+         </a>
+
+         <a href="teste">
+            <img src="https://avatars1.githubusercontent.com/u/62625309?s=460&u=ff8bc1eacb90d3135b28a5bf8e72f4a5d2966b51&v=4" alt="Wanderson Oliveira"/>
+            <div>
+              <strong>rocketseat/unform</strong>
+              <p>Easy peasy highly scalable ReactJs & React Native forms</p>
+            </div>
+
+            <FiChevronRight size={20} />
+         </a>
+       </Repositories>
+    </>
+  )
 }
 
 export default Dashboard;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -20,7 +20,7 @@ export default createGlobalStyle`
   }
 
   #root {
-    max-width: 960px;
+    max-width: 860px;
     margin: 0 auto;
     padding: 40px 20px;
   }
